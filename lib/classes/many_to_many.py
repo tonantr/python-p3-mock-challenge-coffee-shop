87ac1c40ca0be9888d9bc713a9e0862ce42a0db3
tonan <tonantruong@hotmail.com>
2023-08-26 22:15:35 -0400

    completed Coffee Shop

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,82 @@
 class Coffee:
+
+    all = []
+
     def __init__(self, name):
-        self.name = name
-        
+        if len(name) <= 2:
+            raise Exception
+        if type(name) != str:
+            raise Exception
+        self._name = name
+        Coffee.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    
     def orders(self):
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
     def customers(self):
-        pass
+        return list(set(order.customer for order in self.orders()))
     
     def num_orders(self):
-        pass
+        total = 0
+        for order in self.orders():
+            total += 1
+        return total
     
     def average_price(self):
-        pass
+        sum = 0
+        for order in self.orders():
+            sum += order.price
+        return sum / len(self.orders())
 
 class Customer:
+
+    all = []
+
     def __init__(self, name):
-        self.name = name
+        if 15 < len(name) or len(name) < 1:
+            raise Exception
+        self._name = name
+        Customer.all.append(self)
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,value):
+        if type(value) != str:
+            raise Exception
+        self._name = value
         
     def orders(self):
-        pass
+        return [order for order in Order.all if order.customer == self]
     
     def coffees(self):
-        pass
+        return list(set(order.coffee for order in self.orders()))
     
     def create_order(self, coffee, price):
-        pass
+        return Order(self, coffee, price)
     
 class Order:
+
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
-        self.price = price
+        if not isinstance(price, float):
+            raise Exception
+        if 1.0 > price or price > 10.0:
+            raise Exception
+        self._price = price
+        Order.all.append(self)
+    
+    @property
+    def price(self):
+        return self._price
+    
+    
--- lib/testing/customer_test.py
+++ lib/testing/customer_test.py
@@ -22,14 +22,14 @@ class TestCustomer:
         assert customer.name == "Stove"
         
         # comment out the next two lines if using Exceptions
-        customer.name = 1
-        assert customer.name == "Stove"
+        # customer.name = 1
+        # assert customer.name == "Stove"
         
         assert isinstance(customer.name, str)
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     customer.name = 1
+        with pytest.raises(Exception):
+            customer.name = 1
 
     def test_name_length(self):
         """name is between 1 and 15 characters"""
@@ -37,20 +37,20 @@ class TestCustomer:
         assert len(customer.name) == 5
 
         # comment out the next two lines if using Exceptions
-        customer.name = ""
-        assert customer.name == "Steve"
+        # customer.name = ""
+        # assert customer.name == "Steve"
         
         # comment out the next two lines if using Exceptions
-        customer.name = "TooLongForAName!"
-        assert customer.name == "Steve"
+        # customer.name = "TooLongForAName!"
+        # assert customer.name == "Steve"
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Customer("TooLongForAName!")
+        with pytest.raises(Exception):
+            Customer("TooLongForAName!")
 
         # uncomment the next two lines if using Exceptions
-        # with pytest.raises(Exception):
-        #     Customer("")
+        with pytest.raises(Exception):
+            Customer("")
 
     def test_has_many_orders(self):
         """customer has many orders"""
